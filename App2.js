274072d56d60cc9350243638054584829e043b40
yinxiucheng <yinxiucheng@gmail.com>
2019-02-07 17:13:39 +0800

    "添加Header、title"

--- App2.js
+++ App2.js
@@ -11,23 +11,13 @@ import { View, Text, Button, StyleSheet } from "react-native";
 import { createStackNavigator, createAppContainer, createBottomTabNavigator } from "react-navigation";
 
 class HomeScreen extends React.Component {
-    static navigationOptions = ({ navigation }) => {
-        header: () => null //隐藏头部
-    }
-
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
                 <Text>Home Screen</Text>
                 <Button
                     title="Go to Details"
-                    onPress={() => {
-                        /* 1. Navigate to the Details route with params */
-                        this.props.navigation.navigate('Details', {
-                            itemId: 86,
-                            otherParam: 'anything you want here',
-                        });
-                    }}
+                    onPress={() => this.props.navigation.navigate('Details')}
                 />
             </View>
         );
@@ -35,38 +25,24 @@ class HomeScreen extends React.Component {
 }
 
 class DetailsScreen extends React.Component {
-
-    static navigationOptions = {
-        title: "Details"
-    }
-
-
     render() {
-        /* 2. Get the param, provide a fallback value if not available */
-        const { navigation } = this.props;
-        const itemId = navigation.getParam('itemId', 'NO-ID');
-        const otherParam = navigation.getParam('otherParam', 'some default value');
-
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
                 <Text>Details Screen</Text>
-                <Text>itemId: {JSON.stringify(itemId)}</Text>
-                <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-                <Button
+
+                <Button style={styles.btn}
                     title="Go to Details... again"
-                    onPress={() =>
-                        this.props.navigation.push('Details', {
-                            itemId: Math.floor(Math.random() * 100),
-                        })}
+                    onPress={() => this.props.navigation.push('Details')}
                 />
-                <Button
+                <Button style={styles.btn}
                     title="Go to Home"
                     onPress={() => this.props.navigation.navigate('Home')}
                 />
-                <Button
+                <Button style={styles.btn}
                     title="Go back"
                     onPress={() => this.props.navigation.goBack()}
                 />
+
             </View>
         );
     }
