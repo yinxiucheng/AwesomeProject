ad8c2cf755aa711149da77b858e07e555b2eff42
yinxiucheng <yinxiucheng@gmail.com>
2019-02-07 16:51:42 +0800

    "添加TabNavigator"

--- App.js
+++ App.js
@@ -7,25 +7,111 @@
  */
 
 import React from "react";
-import { View, Text } from "react-native";
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { createStackNavigator, createAppContainer, createBottomTabNavigator } from "react-navigation";
 
 class HomeScreen extends React.Component {
     render() {
         return (
             <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
                 <Text>Home Screen</Text>
+                <Button
+                    title="Go to Details"
+                    onPress={() => this.props.navigation.navigate('Details')}
+                />
             </View>
         );
     }
 }
 
-const AppNavigator = createStackNavigator({
-    Home: {
-        screen: HomeScreen
+class DetailsScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <Text>Details Screen</Text>
+
+                <Button style={styles.btn}
+                    title="Go to Details... again"
+                    onPress={() => this.props.navigation.push('Details')}
+                />
+                <Button style={styles.btn}
+                    title="Go to Home"
+                    onPress={() => this.props.navigation.navigate('Home')}
+                />
+                <Button style={styles.btn}
+                    title="Go back"
+                    onPress={() => this.props.navigation.goBack()}
+                />
+
+            </View>
+        );
     }
+}
+
+
+class SettingsScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <Text>Setting Screen</Text>
+                <Button
+                    title="Go to ProfileScreen"
+                    onPress={() => this.props.navigation.navigate('Details')}
+                />
+            </View>
+        );
+    }
+}
+
+
+class ProfileScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <Text>Details Screen</Text>
+
+                <Button style={styles.btn}
+                        title="Go to ProfileScreen...again"
+                        onPress={() => this.props.navigation.push('Details')}
+                />
+            </View>
+        );
+    }
+}
+
+const HomeStack = createStackNavigator({
+    Home: HomeScreen,
+    Details: DetailsScreen
+},{
+    initialRouteName:"Home"
 });
 
-export default createAppContainer(AppNavigator);
+const SettingsStack = createStackNavigator({
+    Settings: SettingsScreen,
+    Profile: ProfileScreen,
+});
+
+const TabNavigator = createBottomTabNavigator(
+    {
+        Home: HomeStack,
+        Settings: SettingsStack,
+    }
+);
+
+
+const AppContainer = createAppContainer(TabNavigator);
+
+export default class App extends React.Component {
+    render() {
+        return <AppContainer />;
+    }
+}
+
+const styles = StyleSheet.create({
+    btn:{
+        marginTop:20,
+        margin:50
+    }
+})
 
 
